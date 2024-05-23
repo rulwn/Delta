3270860405e175e296b4aad6babca72ea7707a79
raul <eduardito.ochoa@gmail.com>
2024-05-23 13:30:50 -0600

    nav  listo

--- app/src/main/java/delta/medic/mobile/activity_login.kt
+++ app/src/main/java/delta/medic/mobile/activity_login.kt
@@ -1,28 +1,20 @@
 package delta.medic.mobile
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class activity_bienvenida : AppCompatActivity() {
+class activity_login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_bienvenida)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val btnContinuar = findViewById<Button>(R.id.btnContinuar)
-        btnContinuar.setOnClickListener {
-            val intent = Intent(this, activity_login::class.java)
-            startActivity(intent)
         }
     }
 }
