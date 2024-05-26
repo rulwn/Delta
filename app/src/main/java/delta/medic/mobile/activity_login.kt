07b34cf7fd6cd7a326dda81418855fca57021be4
raul <eduardito.ochoa@gmail.com>
2024-05-26 12:14:00 -0600

    arreglos

--- app/src/main/java/delta/medic/mobile/activity_login.kt
+++ app/src/main/java/delta/medic/mobile/activity_login.kt
@@ -1,6 +1,9 @@
 package delta.medic.mobile
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,17 @@ class activity_login : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val btnContinuar = findViewById<Button>(R.id.btnContinuar)
+        btnContinuar.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val txtNotienecuenta = findViewById<TextView>(R.id.txtNoTienesCuenta)
+        txtNotienecuenta.setOnClickListener {
+            val intent = Intent(this, activity_register1::class.java)
+            startActivity(intent)
         }
     }
 }
