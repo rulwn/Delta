9f74ad17f364f82fa031541faba02c2c9605c3af
raul <eduardito.ochoa@gmail.com>
2024-05-26 21:39:07 -0600

    arreglosv2

--- app/src/main/java/delta/medic/mobile/activity_register2.kt
+++ app/src/main/java/delta/medic/mobile/activity_register2.kt
@@ -1,6 +1,8 @@
 package delta.medic.mobile
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class activity_register2 : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnSiguiente = findViewById<Button>(R.id.btnSiguiente2)
+        btnSiguiente.setOnClickListener {
+            val intent = Intent(this, activity_register3::class.java)
+            startActivity(intent)
         }
     }
 }
