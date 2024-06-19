d148f6850ba8992a2563e3e27d4055d856431925
raul <eduardito.ochoa@gmail.com>
2024-06-19 15:37:32 -0600

    resolvi roots

--- app/src/main/java/delta/medic/mobile/fragment_usuario.kt
+++ app/src/main/java/delta/medic/mobile/fragment_usuario.kt
@@ -33,7 +33,7 @@ class fragment_usuario : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val root = inflater.inflate(R.layout.fragment_usuario, container, false)
         return root
     }
 
