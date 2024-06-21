0d80e67814cb1864c417374726086aeac9517d68
Luz Gasparío <20200448@ricaldone.edu.sv>
2024-06-20 19:27:48 -0600

    arreglos rama

--- app/src/main/java/delta/medic/mobile/activity_agendar.kt
+++ app/src/main/java/delta/medic/mobile/activity_agendar.kt
@@ -42,20 +42,21 @@ class activity_agendar : AppCompatActivity() {
         val txtFecha = findViewById<TextView>(R.id.txtFecha)
         val rcvSucursal = findViewById<RecyclerView>(R.id.rcvCentro)
         val rcvPaciente = findViewById<RecyclerView>(R.id.rcvPaciente)
-/*
-        val rcvProductos = findViewById<RecyclerView>(R.id.rcvSucursal)
+
+
+        val rcvProductos = findViewById<RecyclerView>(R.id.rcvCentro)
         rcvProductos.layoutManager = LinearLayoutManager(this)
         val layoutNoDoctor = findViewById<LinearLayout>(R.id.linearNoDoctor)
         val rcvCentro = findViewById<RecyclerView>(R.id.rcvCentro)
         rcvCentro.layoutManager = LinearLayoutManager(this)
-
+/*
         val btnEligeCentro = findViewById<ImageButton>(R.id.btnSelecSucursal)
         val doctorExists = checkIfDoctorExists()
 
         if (doctorExists) {
             layoutNoDoctor.visibility = View.GONE
             rcvCentro.visibility = View.VISIBLE
-           // loadSucursalData()
+            // loadSucursalData()
         } else {
             // Doctor data is not present, show the ImageButton layout
             layoutNoDoctor.visibility = View.VISIBLE
@@ -67,31 +68,30 @@ class activity_agendar : AppCompatActivity() {
             startActivityForResult(intent, REQUEST_CODE)
         }
 */
-        /*
+/*
         fun obtenerDatos(): List<dataClassSucursal>
         val rcvCentroMedico = findViewById<RecyclerView>(R.id.rcvCentro)
         rcvCentroMedico.layoutManager = LinearLayoutManager(this)
 
-        fun obtenerDatos(): List<dataClassCentro>
-        {
+        fun obtenerDatos(): List<dataClassCentro> {
             val objConexion = ClaseConexion().CadenaConexion()
             val statement = objConexion?.createStatement()
-            val resultset = statement?.executeQuery("insert into tbCentroMedico (favorito, ID_Doctor, ID_Sucursal) values (?, ?, ?)")!!
+            val resultset =
+                statement?.executeQuery("insert into tbCentroMedico (favorito, ID_Doctor, ID_Sucursal) values (?, ?, ?)")!!
             val centros = mutableListOf<dataClassCentro>()
-            while (resultset.next())
-            {
+            while (resultset.next()) {
 
             }
             return centros
         }
         CoroutineScope(Dispatchers.IO).launch {
             val centroMedico = obtenerDatos()
-            withContext(Dispatchers.Main){
+            withContext(Dispatchers.Main) {
                 val miAdapter = AdaptadorCentro(centroMedico)
                 rcvCentro.adapter = miAdapter
             }
         }
-
+*/
         btnRegresar.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             intent.putExtra("ir_atras", true)
@@ -104,8 +104,8 @@ class activity_agendar : AppCompatActivity() {
     }
 
 
-//Dialog customizado en tamaño
-private fun showCustomDialog() {
+    //Dialog customizado en tamaño
+    private fun showCustomDialog() {
         val dialog = Dialog(this, R.style.CustomDialog)
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dialog.setContentView(R.layout.dialog_customizado)
@@ -132,31 +132,13 @@ private fun showCustomDialog() {
         }
 
         dialog.show()
-}
+    }
 
-
+/*
     private fun checkIfDoctorExists(): Boolean {
         // Implement your logic to check if the doctor data exists
         // For demonstration purposes, we return false to show the layoutNoDoctor
         return false
-    }
-
-    /*private fun loadSucursalData() {
-        CoroutineScope(Dispatchers.IO).launch {
-            val centro = obtenerDatos()
-            withContext(Dispatchers.Main) {
-                val miAdapter = ViewHolderCentro.AdaptadorCentro(centro) { centro ->
-                    val intent = Intent(this@activity_agendar, fragment_Resultados::class.java)
-                    intent.putExtra("ID_Centro", centro.id)
-                    startActivityForResult(intent, REQUEST_CODE)
-                }
-                rcvCentro.adapter = miAdapter
-            }
-        }
-    }
-
-    companion object {
-        const val REQUEST_CODE = 1
     }
 
     private fun loadSucursalData() {
@@ -177,4 +159,3 @@ private fun showCustomDialog() {
         const val REQUEST_CODE = 1
     }
 }
-
