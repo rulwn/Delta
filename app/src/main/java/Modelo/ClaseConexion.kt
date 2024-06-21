31b7fad87ba705de667e7428f66f00406be7f8b1
Alex_Dev <alekeymurcia@gmail.com>
2024-06-20 21:34:51 -0600

    Modificación foto perfil

--- app/src/main/java/Modelo/ClaseConexion.kt
+++ app/src/main/java/Modelo/ClaseConexion.kt
@@ -6,7 +6,7 @@ import java.sql.DriverManager
 class ClaseConexion {
     fun CadenaConexion(): Connection? {
         try {
-            val url = "jdbc:oracle:thin:@192.168.1.12:1521:xe"
+            val url = "jdbc:oracle:thin:@192.168.1.6:1521:xe"
             val usuario = "DeltaMed"
             val clave = "deltaTeam1"
             val conexion = DriverManager.getConnection(url,usuario,clave)
@@ -30,7 +30,7 @@ Luz
 
 
 Hector
-
+val url = "jdbc:oracle:thin:@192.168.1.6:1521:xe"
 
 Jorge
 "jdbc:oracle:thin:@192.168.1.5:1521:xe"
