f57d19b3d89c86612822344f5e724c7ee8fbbfd0
Fernando <jfer.orantes@gmail.com>
2024-06-20 20:12:07 -0600

    commit de prueba

--- app/src/main/java/delta/medic/mobile/fragment_Resultados.kt
+++ app/src/main/java/delta/medic/mobile/fragment_Resultados.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 class fragment_Resultados : Fragment() {
+
+    private lateinit var txtResultadoBusqueda : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
