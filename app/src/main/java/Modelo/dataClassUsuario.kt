795afefba03fabf2c7ddd10a269577abf96a9b14
Alex_Dev <alekeymurcia@gmail.com>
2024-06-22 00:15:41 -0600

    si

--- app/src/main/java/Modelo/dataClassUsuario.kt
+++ app/src/main/java/Modelo/dataClassUsuario.kt
@@ -1,5 +1,20 @@
 package Modelo
 
+import oracle.sql.BLOB
+import oracle.sql.DATE
+import java.sql.Blob
+import java.sql.Date
+
 data class dataClassUsuario(
-    var
+    var idUsuario: Number,
+    var nombreUsuario: String,
+    var apellidoUsuario: String,
+    var emailUsuario: String,
+    var contraseña: String,
+    var dirección: String,
+    var sexo: Char,
+    var fechaNacimiento: Date,
+    var imgUsuario: Blob,
+    var idTipoUsuario: Number,
+    var idSeguro: Number
 )
