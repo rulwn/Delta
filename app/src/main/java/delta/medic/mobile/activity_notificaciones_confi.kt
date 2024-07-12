0a31e8b7a0a165c3b0e6639c870fa86fca872d21
guapurita <guapura956@gmail.com>
2024-07-12 11:10:11 -0600

    Cambios

--- app/src/main/java/delta/medic/mobile/activity_notificaciones_confi.kt
+++ app/src/main/java/delta/medic/mobile/activity_notificaciones_confi.kt
@@ -1,10 +1,13 @@
 package delta.medic.mobile
 
 import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -21,6 +24,19 @@ class activity_notificaciones_confi : AppCompatActivity() {
             insets
         }
         val btnRegresar = findViewById<ImageView>(R.id.btnRegresar)
+        val txtNotificacionesConfi = findViewById<TextView>(R.id.txtNotificacionesConfi)
+
+        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        when (currentNightMode) {
+            Configuration.UI_MODE_NIGHT_NO -> {
+                txtNotificacionesConfi.setTextColor(ContextCompat.getColor(this, R.color.black))
+                btnRegresar.setColorFilter(ContextCompat.getColor(this, R.color.black))
+            } // Night mode is not active, we're using the light theme.
+            Configuration.UI_MODE_NIGHT_YES -> {
+                txtNotificacionesConfi.setTextColor(ContextCompat.getColor(this, R.color.white))
+                btnRegresar.setColorFilter(ContextCompat.getColor(this, R.color.white))
+            } // Night mode is active, we're using dark theme.
+        }
         btnRegresar.setOnClickListener {
             finish()
         }
