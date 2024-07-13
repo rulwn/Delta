26896afd9cdd5776187722a0fe18df8c05c7b4f6
Fernando <jfer.orantes@gmail.com>
2024-07-13 03:03:29 -0600

    nose si funciona el ver notificaciones

---
+++ app/src/main/java/Modelo/dataClassNotis.kt
@@ -0,0 +1,10 @@
+package Modelo
+
+class dataClassNotis (
+    val idNoti: Int,
+    val title: String,
+    val subtitle: String,
+    val tiponoti: String,
+    val date: String,
+    val userId: Int
+)
--- app/src/main/java/delta/medic/mobile/fragment_notificaciones.kt
+++ app/src/main/java/delta/medic/mobile/fragment_notificaciones.kt
@@ -1,5 +1,6 @@
 package delta.medic.mobile
 
+import Modelo.AdaptadorNotis
 import android.os.Bundle
 import android.text.Html
 import androidx.fragment.app.Fragment
@@ -7,11 +8,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import Modelo.ClaseConexion
+import Modelo.dataClassNotis
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+
 
 /**
  * A simple [Fragment] subclass.
@@ -19,45 +25,67 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class fragment_notificaciones : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: AdaptadorNotis
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_notificaciones, container, false)
-        val lbNotificaciones = root.findViewById<TextView>(R.id.lbNotis)
-        lbNotificaciones.setText(Html.fromHtml(getResources().getString(R.string.lbNotificacionesSub)))
+
+        recyclerView = root.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        loadNotifications()
+
         return root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment fragment_notificaciones.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            fragment_notificaciones().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun loadNotifications() {
+        CoroutineScope(Dispatchers.Main).launch {
+            val notifications = withContext(Dispatchers.IO) {
+                getNotificationsFromDatabase()
             }
+            adapter = AdaptadorNotis(notifications)
+            recyclerView.adapter = adapter
+        }
     }
+
+    private suspend fun getNotificationsFromDatabase(): List<dataClassNotis> {
+        return withContext(Dispatchers.IO) {
+            val notifications = mutableListOf<dataClassNotis>()
+            try {
+                val connection = ClaseConexion().cadenaConexion()
+                if (connection != null) {
+                    val statement = connection.prepareStatement("SELECT ID_Notificacion, mensajeNoti AS title, tipoNoti AS type, TO_CHAR(fechaNoti, 'YYYY-MM-DD') AS date, ID_Usuario, '' AS subtitle FROM tbNotis WHERE ID_Usuario = ?")!!
+                    statement.setInt(1, 1) // aqui asumo q 1 es el numero de tipo de usuario
+                    val resultSet = statement.executeQuery()
+                    while (resultSet.next()) {
+                        val idNotis = resultSet.getInt("ID_Notificacion")
+                        val title = resultSet.getString("title")
+                        val subtitle = resultSet.getString("subtitle")
+                        val type = resultSet.getString("type")
+                        val date = resultSet.getString("date")
+                        val userId = resultSet.getInt("ID_Usuario")
+                        val notification = dataClassNotis(idNotis, title, subtitle, type, date, userId)
+                        notifications.add(notification)
+                    }
+                    resultSet.close()
+                    statement.close()
+                    connection.close()
+                } else {
+                    println("No se pudo establecer conexión con la base de datos.")
+                }
+            } catch (e: Exception) {
+                println("Este es el error ${e.message}")
+            }
+            notifications
+        }
+    }
+
+
 }
