c693c08a05f1b95050f94c1ff7782639e516d139
Alex_Dev <139160462+AlexGameDev07@users.noreply.github.com>
2024-10-05 18:53:44 -0600

    cambios base

--- DB_DeltaMed.sql
+++ DB_DeltaMed.sql
@@ -605,7 +605,7 @@ CREATE TABLE tbSeguros (
     ON DELETE CASCADE,
 
     CONSTRAINT FK_Seguro FOREIGN KEY (ID_Usuario)
-    REFERENCES tbUsuarios(ID_Usuario)
+    REFERENCES Tbusuarios(ID_Usuario)
     ON DELETE CASCADE
 );
 
@@ -1642,16 +1642,19 @@ SELECT DUMMY FROM DUAL;
 
 INSERT ALL
     INTO tbCitasMedicas (diaCita, horaCita, motivo, estadoCita, ID_Doctor, ID_Usuario)
-         VALUES (TO_DATE('2024-10-01', 'YYYY-MM-DD'), TO_TIMESTAMP('2023-01-01 10:00:00', 'YYYY-MM-DD HH24:MI:SS'), 'Consulta general','A', 5, 1)
+         VALUES (TO_DATE('2024-10-01', 'YYYY-MM-DD'), TO_TIMESTAMP('2023-01-01 10:00:00', 'YYYY-MM-DD HH24:MI:SS'), 'Consulta general','A', 1, 1)
     INTO tbCitasMedicas (diaCita, horaCita, motivo, estadoCita, ID_Doctor, ID_Usuario)
-         VALUES (TO_DATE('2024-10-02', 'YYYY-MM-DD'), TO_TIMESTAMP('2023-01-02 11:00:00', 'YYYY-MM-DD HH24:MI:SS'), 'Revisi√≥n anual','A', 4, 2)
+         VALUES (TO_DATE('2024-10-02', 'YYYY-MM-DD'), TO_TIMESTAMP('2023-01-02 11:00:00', 'YYYY-MM-DD HH24:MI:SS'), 'Revisi√≥n anual','A', 1, 2)
     INTO tbCitasMedicas (diaCita, horaCita, motivo, estadoCita, ID_Doctor, ID_Usuario)
          VALUES (TO_DATE('2024-10-03', 'YYYY-MM-DD'), TO_TIMESTAMP('2023-01-03 12:00:00', 'YYYY-MM-DD HH24:MI:SS'), 'Consulta de seguimiento','A', 3, 3)
     INTO tbCitasMedicas (diaCita, horaCita, motivo, estadoCita, ID_Doctor, ID_Usuario)
          VALUES (TO_DATE('2024-10-04', 'YYYY-MM-DD'), TO_TIMESTAMP('2023-01-04 13:00:00', 'YYYY-MM-DD HH24:MI:SS'), 'Consulta general','A', 2, 4)
     INTO tbCitasMedicas (diaCita, horaCita, motivo, estadoCita, ID_Doctor, ID_Usuario)
-         VALUES (TO_DATE('2024-10-05', 'YYYY-MM-DD'), TO_TIMESTAMP('2023-01-05 14:00:00', 'YYYY-MM-DD HH24:MI:SS'), 'Consulta especializada','A', 1, 5)
+         VALUES (TO_DATE('2024-11-08', 'YYYY-MM-DD'), TO_TIMESTAMP('2024-11-08 14:00:00', 'YYYY-MM-DD HH24:MI:SS'), 'Consulta especializada','A', 1, 5)
 SELECT DUMMY FROM DUAL;
+Insert
+    INTO tbCitasMedicas (diaCita, horaCita, motivo, estadoCita, ID_Doctor, ID_Usuario)
+         VALUES (TO_DATE('2024-11-08', 'YYYY-MM-DD'), TO_TIMESTAMP('2024-11-08 14:00:00', 'YYYY-MM-DD HH24:MI:SS'), 'Consulta especializada','A', 1, 5); commit;
 
 INSERT ALL
     INTO tbNotis (fechaNoti, tipoNoti, mensajeNoti, flag, ID_Usuario, ID_TipoNoti) 
@@ -2004,7 +2007,7 @@ LEFT JOIN (
     GROUP BY r.promEstrellas
 ) r ON e.PromEstrellas = r.PromEstrellas
 ORDER BY e.PromEstrellas;
-
+    
 WITH Meses AS (
     -- Genera 12 meses (5 hacia atr·s y 6 hacia adelante incluyendo el actual)
     SELECT ADD_MONTHS(TRUNC(SYSDATE, 'MM'), LEVEL - 6) AS Mes
@@ -2012,11 +2015,11 @@ WITH Meses AS (
     CONNECT BY LEVEL <= 12
 )
 SELECT 
-    TO_CHAR(m.Mes, 'YYYY-MM') AS AÒoMes,                     -- AÒo y mes en formato AÒo-Mes
-    TO_CHAR(m.Mes, 'Month YYYY', 'NLS_DATE_LANGUAGE=SPANISH') AS NombreMes, -- Nombre del mes con aÒo
-    NVL(COUNT(c.ID_Cita), 0) AS Total_Citas,                 -- Total de citas
-    NVL(SUM(CASE WHEN c.estadoCita = 'A' THEN 1 ELSE 0 END), 0) AS Citas_Activas, -- Citas activas
-    NVL(SUM(CASE WHEN c.estadoCita = 'C' THEN 1 ELSE 0 END), 0) AS Citas_Inactivas -- Citas inactivas
+    TO_CHAR(m.Mes, 'YYYY-MM') AS AÒoMes,                     
+    TO_CHAR(m.Mes, 'Month YYYY', 'NLS_DATE_LANGUAGE=SPANISH') AS NombreMes,
+    NVL(COUNT(c.ID_Cita), 0) AS Total_Citas,                
+    NVL(SUM(CASE WHEN c.estadoCita = 'A' THEN 1 ELSE 0 END), 0) AS Citas_Activas, 
+    NVL(SUM(CASE WHEN c.estadoCita = 'C' THEN 1 ELSE 0 END), 0) AS Citas_Inactivas
 FROM 
     Meses m
 LEFT JOIN 
@@ -2024,12 +2027,66 @@ LEFT JOIN
 ON 
     TO_CHAR(c.diaCita, 'YYYY-MM') = TO_CHAR(m.Mes, 'YYYY-MM')
 LEFT JOIN 
-    tbDoctores d ON c.ID_Doctor = d.ID_Doctor
+    tbDoctores d 
+ON 
+    c.ID_Doctor = d.ID_Doctor
 LEFT JOIN 
-    tbUsuarios u ON d.ID_Usuario = u.ID_Usuario group by TO_CHAR(m.Mes, 'YYYY-MM'), TO_CHAR(m.Mes, 'Month YYYY', 'NLS_DATE_LANGUAGE=SPANISH')
+    tbUsuarios u 
+ON 
+    d.ID_Usuario = u.ID_Usuario
 WHERE 
-    u.emailUsuario = 'xam@gmail.com'  -- Filtra por el correo electrÛnico del usuario
+    u.emailUsuario = 'xam@gmail.com'  -- Filtro por el correo del doctor
+    OR u.emailUsuario IS NULL  -- Esto permite mantener los meses sin citas
 GROUP BY 
     m.Mes
 ORDER BY 
     m.Mes;
+
+SELECT (U.nombreUsuario || ' ' || U.apellidoUsuario) AS NombreUsuario, 
+       C.Motivo, C.HoraCita, U.imgUsuario 
+FROM tbCitasMedicas C 
+INNER JOIN tbUsuarios U ON U.ID_Usuario = C.ID_Usuario 
+WHERE EXISTS ( 
+    SELECT 1 
+    FROM tbDoctores D 
+    INNER JOIN tbUsuarios U2 ON U2.ID_Usuario = D.ID_Usuario 
+    WHERE U2.emailUsuario = 'xam@gmail.com' 
+    AND D.ID_Doctor = C.ID_Doctor 
+) 
+AND C.horacita > CURRENT_TIMESTAMP 
+AND C.ESTADOCITA = 'A'
+ORDER BY C.diaCita ASC
+FETCH FIRST 3 ROWS ONLY;
+
+Select ID_Doctor, emailusuario, NombreUsuario from tbDoctores r INNER JOIN tbUSuarios u On u.ID_Usuario = r.ID_Usuario;
+
+    SELECT 
+        (SUM(r.PromEstrellas) / NULLIF(COUNT(r.PromEstrellas), 0)) AS "Promedio", 
+        COUNT(r.PromEstrellas) * 5 AS "Total", 
+        (SUM(r.PromEstrellas) * 100 / (COUNT(r.PromEstrellas) * 5)) || '%' AS "Porcentaje"
+    FROM 
+        tbReviews r
+    WHERE 
+        EXISTS (
+            SELECT 1 
+            FROM tbDoctores d
+            INNER JOIN tbUsuarios u ON u.ID_Usuario = d.ID_Usuario 
+            WHERE u.emailUsuario = 'orantes@gmail.com' 
+            AND d.ID_Doctor = r.ID_Doctor
+        );
+
+SELECT 
+    u.NombreUsuario || ' ' || u.ApellidoUsuario AS Doctor,
+    e.NombreEspecialidad as Especialidad,
+    u.ImgUsuario AS ImagenDoctor,
+    s.ImgSucursal AS ImagenSucursal,
+    s.ID_Sucursal AS Sucursal
+FROM 
+    tbDoctores d
+INNER JOIN 
+    tbUsuarios u ON d.ID_Usuario = u.ID_Usuario
+INNER JOIN
+    tbSucursales s ON d.ID_Sucursal = s.ID_Sucursal
+INNER JOIN
+    tbEspecialidades e ON d.ID_Especialidad = e.ID_Especialidad
+WHERE u.emailUsuario = 'xam@gmail.com';
